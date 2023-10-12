2b2f36da72658db294cd531bcc6764b438ba8d5a
Anton1oC4astr0 <143725409+Anton1oC4astr0@users.noreply.github.com>
2023-10-11 22:04:44 -0600

    Add files via upload

---
+++ Practica3Fibonacci.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica3fibonacci;
+import java.util.Scanner;
+/**
+ *
+ * @author Usuario
+ */
+public class Practica3Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int a = 0, b = 1, c, n;
+        System.out.print("Ingresa la cantidad de numeros para la serie: ");
+        n = scanner.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println(a);
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+    
+}
